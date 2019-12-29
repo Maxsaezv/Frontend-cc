9ca3b65f172d5c1c4adf878e30bc17fb56e43e72
Diex <aguilera.diego@live.cl>
2019-12-29 19:04:36 -0300

    Update Header.js

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,15 +1,52 @@
-import React, { Component } from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import HomeIcon from '@material-ui/icons/Home';
+import { Link } from '@material-ui/core';
+import GroupIcon from '@material-ui/icons/Group';
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-    this.state = {};
-  }
+export default function ButtonAppBar() {
+  const classes = useStyles();
 
-  render() {
-    return <div>Header</div>;
-  }
-}
-
-export default Header;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+        <Button color="inherit">
+          <Link href="javascript:window.history.back()" color='inherit'>
+          <i className="material-icons left">keyboard_backspace</i>
+          </Link>
+          </Button>
+          <Typography variant="h6" className={classes.title}>
+            Medical Management Reporter
+          </Typography>
+          <Button color="inherit">
+            <Link href='/plantel' color="inherit">
+            <GroupIcon />Plantel
+              </Link>
+          </Button>
+          <Button color="inherit">
+            <Link href='/dashboard' color="inherit">
+            <HomeIcon />Inicio
+              </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
